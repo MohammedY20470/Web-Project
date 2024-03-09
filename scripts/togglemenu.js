6a3a9cae23eb7ee5b4657cfe2a1c6936c4da4ea8
MohammedY20470 <ymsmohammed@gmail.com>
2024-03-10 00:22:47 +0300

    First Project

---
+++ scripts/togglemenu.js
@@ -0,0 +1,8 @@
+function toggleLinks() {
+    var menu = document.getElementById('menu-links');
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
